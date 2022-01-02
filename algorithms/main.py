7ac59656da1225f0773a29aa0c62cad608d2b5e0
Georgette <92040710+MlleGeorgette@users.noreply.github.com>
2022-01-02 17:11:39 +0000

    Add files via upload

---
+++ algorithms/main.py
@@ -0,0 +1,11 @@
+def solveMeFirst(a, b):
+    a = int(input())
+    b = int(input())
+    c = a + b
+    # res = solveMeFirst(a, b)
+    # print(res)
+    print(c)
+    return c
+
+
+solveMeFirst(5, 2)
---
+++ arrays/jan02_2021.py
@@ -0,0 +1,37 @@
+"""
+This tutorial shows the difference between a list and an array.
+A list is useful for storing simple data where no mathematical calculation is expected.
+However, arrays are more efficient for storing large datasets and for performing calculations.
+Source: https://learnpython.com/blog/python-array-vs-list/
+"""
+import array as arr
+import numpy as np
+
+# Array using array module
+array_1 = arr.array("i", [2, 4, 6, 8]) # "i" means all items in the array are integers
+print(array_1)
+print(type(array_1))
+
+# Array using numpy module. Numpy arrays can have elements of different data type
+array_2 = np.array(["string", 2, 4, 6, 8])
+print(array_2)
+print(type(array_2))
+
+# Division using a numpy array
+array_3 = np.array([10, 20, 30, 40])
+division = array_3/2
+print(division)
+print(type(division))
+
+# Cf the above with Python's built-in lists. No need to declare the data structure as a list
+list_1 = [2, 4, 6, 8]
+print(list_1)
+print(type(list_1))
+
+# Much less efficient to do a division with a list
+division = []
+for i in range(len(list_1)):
+    result = list_1[i]/2
+    division.append(result)
+print(division)
+print(type(division))
