d1003ee7567eab23b8c1a303f0d41d8f44f09af3
MlleGeorgette <92040710+MlleGeorgette@users.noreply.github.com>
2022-03-09 16:31:41 +0000

    Created button and function to randomise font style

--- tkinter/text_editor.py
+++ tkinter/text_editor.py
@@ -2,6 +2,7 @@
 import re
 import tkinter as gui
 from tkinter.filedialog import askopenfilename, asksaveasfilename
+import random
 
 
 # Functions
@@ -36,6 +37,13 @@ def count(event):
     lbl_char_count["text"] = "Characters:\n " + str(len(words))
 
 
+def change_font():
+    font_list = ["Helvetica 12 bold", "Verdana 16", "Cambria 8 bold"]
+    font = random.choice(font_list)
+    text_box['font'] = f"{font}"
+    lbl_font['text'] = f"Font:\n " + str(font)
+
+
 window = gui.Tk()
 window.title("Text Editor")
 window.iconbitmap("yawdie_code_logo.ico")
@@ -50,13 +58,20 @@ btn_save = gui.Button(master=fr_buttons, text="SAVE AS",
                       width=10, bg='#91a18d', command=save_file)
 lbl_word_count = gui.Label(master=fr_buttons, text="Words: ")
 lbl_char_count = gui.Label(master=fr_buttons, text="Characters: ")
+lbl_font = gui.Label(master=fr_buttons, text="Font:\nArial 12 ")
+btn_font = gui.Button(master=fr_buttons, text="CHANGE FONT",
+                      width=10, bg='#91a18d', command=change_font)
+
+# Add buttons and labels to frame1; add frame1 to window
 btn_open.grid(row=1, column=0, sticky="nsew", padx=5, pady=5)
 btn_save.grid(row=2, column=0, sticky="nsew", padx=5, pady=10)
 lbl_word_count.grid(row=4, column=0, sticky="nsew", padx=5, pady=5)
-lbl_char_count.grid(row=6, column=0, sticky="nsew", padx=5, pady=5)
+lbl_char_count.grid(row=8, column=0, sticky="nsew", padx=5, pady=5)
+lbl_font.grid(row=10, column=0, sticky="nsew", padx=5, pady=5)
+btn_font.grid(row=12, column=0, sticky="nsew", padx=5, pady=5)
 fr_buttons.grid(row=0, column=0, sticky="ns")
 
-text_box = gui.Text(master=window, bg='#e6f0e9')
+text_box = gui.Text(master=window, font="Arial 12", bg='#e6f0e9')
 text_box.grid(row=0, column=1, sticky="nsew")
 
 text_box.bind("<KeyPress>", count)
